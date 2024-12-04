23e2a0fd509d7f18bff47bbff010ad37ab00cfa3
hieu1012 <daotaohieu@gmail.com>
2024-12-04 15:56:30 +0700

    Test

---
+++ src/Screens/Test.js
@@ -0,0 +1,35 @@
+import {
+    View,
+    Text,
+    Image,
+    TouchableOpacity,
+    StyleSheet,
+    ScrollView,
+    TextInput,
+    FlatList,
+  } from 'react-native';
+  import {
+    FontAwesome,
+    AntDesign,
+    Ionicons,
+    MaterialIcons,
+    MaterialCommunityIcons,
+    Feather,
+  } from '@expo/vector-icons';
+  import { useState, useEffect } from 'react';
+  
+  export default function Electronics({ navigation }) {
+    return (
+      <View>
+      {/* Thanh Tim Kiem */}
+      {/* Sub Category */}
+      {/* Mục lọc sản phẩm */}
+      {/* Danh  sản phẩm */}
+      </View>
+    );
+  }
+  
+  const styles = StyleSheet.create({
+    
+  });
+  
